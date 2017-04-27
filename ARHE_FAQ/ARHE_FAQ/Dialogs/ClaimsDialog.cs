4a187ede1ec9c4e01f8135ad32be1b75f679f041
jdruid <joshdrew@outlook.com>
2017-04-27 14:50:20 -0500

    Initial dialog Commit

---
+++ ARHE_FAQ/ARHE_FAQ/Dialogs/ClaimsDialog.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Bot.Builder.Dialogs;
+using Microsoft.Bot.Connector;
+
+namespace ARHE_FAQ.Dialogs
+{
+    [Serializable]
+    public class ClaimsDialog : IDialog<object>
+    {
+        public Task StartAsync(IDialogContext context)
+        {
+            context.Wait(MessageReceivedAsync);
+
+            return Task.CompletedTask;
+        }
+
+        private async Task MessageReceivedAsync(IDialogContext context, IAwaitable<object> result)
+        {
+            var activity = await result as IMessageActivity;
+
+            PromptDialog.Confirm(
+               context: context,
+               resume: ResumeAndPromptCustomerAsync,
+               prompt: "Ok. You asked about claims, are you a current customer?",
+               retry: "I didn't understand. Please try again.");
+        }
+
+        private async Task ResumeAndPromptCustomerAsync(IDialogContext context, IAwaitable<bool> result)
+        {
+            bool isCurrentCustomer = await result;          
+           
+            context.Done(GetCustomerClaimInfo(isCurrentCustomer));
+        }
+
+        public string GetCustomerClaimInfo(bool isCurrentCustomer)
+        {
+            if (isCurrentCustomer)
+            {
+                return "Please go to www.arhe.com/current-customer-claims for more info";
+            } else
+            {
+                return "Please go to www.arhe.com/new-customer-claims for more info";
+            }
+        }
+
+       
+    }
+}
--- ARHE_FAQ/ARHE_FAQ/Dialogs/RootDialog.cs
+++ ARHE_FAQ/ARHE_FAQ/Dialogs/RootDialog.cs
@@ -2,30 +2,121 @@
 using System.Threading.Tasks;
 using Microsoft.Bot.Builder.Dialogs;
 using Microsoft.Bot.Connector;
+using System.Linq;
 
 namespace ARHE_FAQ.Dialogs
 {
     [Serializable]
     public class RootDialog : IDialog<object>
     {
+        //Properties used in prompts
+        public string name;
+        public FAQTypes faqtype;
+        public bool IsCustomerRequired = false;
+
         public Task StartAsync(IDialogContext context)
         {
-            context.Wait(MessageReceivedAsync);
+            context.Wait(InitialPromptMessageAsync);
 
             return Task.CompletedTask;
         }
 
-        private async Task MessageReceivedAsync(IDialogContext context, IAwaitable<object> result)
+        private async Task InitialPromptMessageAsync(IDialogContext context, IAwaitable<object> result)
         {
             var activity = await result as Activity;
 
-            // calculate something for us to return
-            int length = (activity.Text ?? string.Empty).Length;
-
-            // return our reply to the user
-            await context.PostAsync($"You sent {activity.Text} which was {length} characters");
-
-            context.Wait(MessageReceivedAsync);
+            //Recived the initial text from the user. What do we want to do?
+            PromptDialog.Text(
+                context: context,
+                resume: ResumeAndPromptFAQAsync,
+                prompt: "Hello, I am the ARHE Bot. I am here to help. Can I please have your name?",
+                retry: "I didn't understand. Please try again.");
+            
         }
+
+        private async Task ResumeAndPromptFAQAsync(IDialogContext context, IAwaitable<string> result)
+        {
+            name = await result;
+
+            PromptDialog.Choice(
+                context: context,
+                resume: ResumeAndPromptDecisionAsync,
+                options: Enum.GetValues(typeof(FAQTypes)).Cast<FAQTypes>().ToArray(),
+                prompt: $"Thank you {name}. Which of these areas do you need help with today?",
+                retry: "I didn't understand. Please try again."
+                );
+            
+        }
+
+        private async Task ResumeAndPromptDecisionAsync(IDialogContext context, IAwaitable<FAQTypes> result)
+        {
+            faqtype = await result;
+
+            //We have the FAQ Type - need to determine if they are a customer or not.
+            switch (faqtype.ToString())
+            {
+                case "Status":
+                    context.Call(new StatusDialog(), ResumeAfterOptionDialog);
+                    break;
+                case "Claims":
+                    context.Call(new ClaimsDialog(), ResumeAfterOptionDialog);
+                    break;
+                case "Reimbursements":
+                    context.Call(new ReimbursementsDialog(), ResumeAfterOptionDialog);
+                    break;
+                case "Appointments":
+                    context.Call(new AppointmentsDialog(), ResumeAfterOptionDialog);
+                    break;
+                default:
+                    break;
+            }
+
+        }
+
+        private async Task ResumeAfterOptionDialog(IDialogContext context, IAwaitable<object> result)
+        {
+            try
+            {
+                var message = await result;
+            }
+            catch (Exception ex)
+            {
+                await context.PostAsync($"Failed with message: {ex.Message}");
+            }
+            finally
+            {
+                context.Wait(this.BackFromDialogPromptAsync);
+            }
+        }
+
+        private async Task BackFromDialogPromptAsync(IDialogContext context, IAwaitable<object> result)
+        {
+            var activity = await result as Activity;
+
+            PromptDialog.Confirm(
+                context: context,
+                resume: ResumeOrEndPromptAsync,
+                prompt: "Is there anything else I can help you with today",
+                retry: "I didn't understand. Please try again.");
+        }
+
+        private Task ResumeOrEndPromptAsync(IDialogContext context, IAwaitable<bool> result)
+        {
+            throw new NotImplementedException();
+        }
+
+
+
+        #region ENUMS for Choices
+        public enum FAQTypes
+        {
+            Status,
+            Claims,
+            Reimbursements,
+            Appointments
+        }
+        #endregion
+
+        
     }
 }
