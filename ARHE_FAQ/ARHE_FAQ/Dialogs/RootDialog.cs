93d25445204eb7842e68d7cd1992da955252ca92
jdruid <joshdrew@outlook.com>
2017-04-28 09:22:51 -0500

    commit root

--- ARHE_FAQ/ARHE_FAQ/Dialogs/RootDialog.cs
+++ ARHE_FAQ/ARHE_FAQ/Dialogs/RootDialog.cs
@@ -83,10 +83,10 @@ namespace ARHE_FAQ.Dialogs
                     context.Call(new ClaimsDialog(), BackFromDialogPrompt);
                     break;
                 case "Reimbursements":
-                    //context.Call(new ReimbursementsDialog(), BackFromDialogPrompt);
+                   context.Call(new ReimbursementsDialog(), BackFromDialogPrompt);
                     break;
                 case "Appointments":
-                    //context.Call(new AppointmentsDialog(), BackFromDialogPrompt);
+                    context.Call(new AppointmentsDialog(), BackFromDialogPrompt);
                     break;
                 default:
                     break;
